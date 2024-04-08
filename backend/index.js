bcf390d6f7c3044aff00e7bd7b334215061cd474
Kapil619 <kapilbadokar321@gmail.com>
2024-04-08 09:28:24 +0530

    created API for crud operations

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
-import express, { request } from "express"
+import express, { request, response } from "express"
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
+
+
 const app = express();
 
 
@@ -11,6 +13,8 @@ app.get('/', (request, response) => {
     return response.status(234).send('WELCOME Kapil');
 })
 
+
+//CREATING A BOOK
 app.post('/books', async (request, response) => {
     try {
         if (!request.body.title || !request.body.author || !request.body.publishYear) {
@@ -32,7 +36,77 @@ app.post('/books', async (request, response) => {
     }
 })
 
+//GETTING ALL BOOKS
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
 
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    }
+    catch (error) {
+        console.log(error);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+//GETTING A SINGLE BOOK
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    }
+    catch (error) {
+        console.log(error);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+//UPDATING A BOOK
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (!request.body.title || !request.body.author || !request.body.publishYear) {
+            return response.status(400).send({ message: 'All fields are required' });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).send({ message: 'Book not found' });
+        }
+        return response.status(200).send({ message: 'Book updated successfully' });
+    }
+    catch (error) {
+        console.log(error);
+        response.status(500).send({ message: error.message });
+    }
+
+})
+
+//DELETING A BOOK
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const result = await Book.findByIdAndUpdate(id);
+
+        if (!result) {
+            return response.status(404).send({ message: 'Book not found' });
+        }
+        return response.status(200).send({ message: 'Book deleted successfully' });
+    }
+    catch (error) {
+        console.log(error);
+        response.status(500).send({ message: error.message });
+
+    }
+});
 
 mongoose.connect(mongoDBURL).then(() => {
     console.log('Connected to MongoDB');
