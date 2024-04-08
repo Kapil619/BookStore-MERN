072e7854627f4fbe17de2a49b4327433868a85b7
Kapil619 <kapilbadokar321@gmail.com>
2024-04-08 20:01:45 +0530

    Added axios and react-icons &  Displaying table to show books on frontend

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,11 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+// }));
 
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import EditBook from './pages/EditBook'
-import CreateBook from './pages/CreateBook'
+import CreateBooks from './pages/CreateBooks'
 import DeleteBook from './pages/DeleteBook'
 import ShowBook from './pages/ShowBook'
 
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/create" element={<CreateBooks />} />
       <Route path="/books/details/:id" element={<ShowBook />} />
       <Route path="/books/edit/:id" element={<EditBook />} />
       <Route path="/books/delete/:id" element={<DeleteBook />} />
