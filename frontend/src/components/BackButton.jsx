c8e9582507a24b217194c12cdcd2ad688e83be52
Kapil619 <kapilbadokar321@gmail.com>
2024-04-09 00:42:42 +0530

    Showing  book details.

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+import { BsArrowLeft } from "react-icons/bs"
+
+
+const BackButton = ({ destination = '/' }) => {
+    return (
+        <div className="flex" >
+            <Link to={destination} className="bg-sky-800 text-white px-4 rounded-lg py-1 w-fit" >
+                <BsArrowLeft className="text-2xl" />
+            </Link>
+
+        </div>
+    )
+}
+
+export default BackButton
