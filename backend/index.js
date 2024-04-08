9e1034f3d7832a8ad8d9d2fe0d74499c87efe9cf
Kapil619 <kapilbadokar321@gmail.com>
2024-04-08 09:48:34 +0530

    configured cors

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,20 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
-
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
+
 
 
 app.get('/', (request, response) => {
